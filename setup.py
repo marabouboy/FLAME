964f721beadeb49f9dfd6b6da776f65544097162
marabouboy <51740261+marabouboy@users.noreply.github.com>
2021-09-22 13:38:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="FLAME: Full Length Adjecency Matrix Enumeration", # Replace with your own username
-    version="0.0.1",
+    version="0.3.1",
     author="Alan Bäckerholm",
     author_email="Alan.baek1@gmail.com",
     description="FLAME: Full Length Adjecency Matrix Enumeration - is a longread splice variant annotation tool that allows for the filtering and quantification of splice variants according to the gene annotation file. It also allows for the detection and confirmation of novel exons and/or splice variants through the use of adjecent nucleotide in the reference and through the use of shortread sequencing",
